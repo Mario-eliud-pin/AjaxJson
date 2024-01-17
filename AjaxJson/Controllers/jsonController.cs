a830d94d435dcd24a6eb03f984c8f7940e687b03
Mario-eliud-pin <martandroy@hotmail.com>
2024-01-16 21:28:24 -0600

    Agregar archivos de proyecto.

---
+++ AjaxJson/Controllers/jsonController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AjaxJson.Models;
+
+namespace AjaxJson.Controllers
+{
+    public class jsonController : Controller
+    {
+        // GET: json
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult ObtenerPersona()
+        {
+            List<persona> lst;
+            using (ajaxJsonEntities db = new ajaxJsonEntities())
+            {
+                lst = (from d in db.persona select d).ToList();
+            }
+            return Json(lst, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
